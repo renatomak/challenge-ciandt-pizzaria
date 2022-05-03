b144e177995655a8e3ef5bd8deba909bb30d12f0
renatomak <renato.mark.silva@gmail.com>
2022-05-03 17:40:50 -0300

    feat: messages

---
+++ src/main/java/com/ciandt/pizzaria/utils/Messages.java
@@ -0,0 +1,12 @@
+package com.ciandt.pizzaria.utils;
+
+public class Messages {
+    public static final String VALIDATION_NAME_IS_REQUIRED = "Name is required";
+    public static final String VALIDATION_DESCRIPTION_IS_REQUIRED = "Description is required";
+    public static final String VALIDATION_PRICE_IS_REQUIRED = "Price is required";
+    public static final String EXCEPTION_RESOURCE_NOT_FOUND = "Resource not found";
+    public static final String EXCEPTION_DATABASE_EXCEPTION = "Database exception";
+    public static final String EXCEPTION_INTEGRITY_VIOLATION = "Integrity violation";
+    public static final String EXCEPTION_ID_NOT_FOUND = "Id not found ";
+    public static final String EXCEPTION_ENTITY_NOT_FOUND = "Entity not found";
+}
