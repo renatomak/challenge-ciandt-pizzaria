6d37ae99a7bddffcb41bd35a7529f3862ff358da
renatomak <renato.mark.silva@gmail.com>
2022-05-03 07:57:28 -0300

    first commit

---
+++ src/main/java/com/ciandt/pizzaria/PizzariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ciandt.pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaApiApplication.class, args);
+	}
+
+}
