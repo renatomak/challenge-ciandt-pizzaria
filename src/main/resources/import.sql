ca71552803c80a943cf8b1b550194d131d526e6f
renatomak <renato.mark.silva@gmail.com>
2022-05-03 08:25:19 -0300

    feat: import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_flavor (description, name, price) VALUES ('Molho de tomate, bacon, cebola, azeitona, mussarela e orégano.', 'Acebolada', 30.00);
+INSERT INTO tb_flavor (description, name, price) VALUES ('Molho de tomate, mussarela, alho, óleo e orégano.', 'Alho e Óleo', 30.00);
+INSERT INTO tb_flavor (description, name, price) VALUES ('Molho de tomate, bacon, ovos, cebola, milho, mussarela e orégano.', 'Americana', 30.00);
+INSERT INTO tb_flavor (description, name, price) VALUES ('Molho de tomate, mussarela, calabresa, frango, milho, palmito, catupiri e orégano.', 'Apetitosa', 30.00);
+INSERT INTO tb_flavor (description, name, price) VALUES ('Molho de tomate, bacon, mussarela e orégano.', 'Romana', 30.00);
+INSERT INTO tb_flavor (description, name, price) VALUES ('Molho de tomate, mussarela, calabresa, e orégano', 'Bacon', 30.00);
