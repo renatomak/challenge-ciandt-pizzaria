4884603328ca90cb70a82212d44f59e8d66f5a6e
renatomak <renato.mark.silva@gmail.com>
2022-05-03 08:38:30 -0300

    feat: exception handle

---
+++ src/main/java/com/ciandt/pizzaria/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ciandt.pizzaria.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 2062434147875331065L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
